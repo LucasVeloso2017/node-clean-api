f769250cd32452347a29cb2310ec9339271aea8f
Lucas Veloso <lucas.velloso2017@gmail.com>
2022-01-05 22:31:04 -0300

    refactor: add depency injection for email validation and make validation

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,10 +1,16 @@
+import { InvalidParamError } from './../errors/invalid-param-error'
 
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-param-error'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 
 export class SignupController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -12,6 +18,12 @@ export class SignupController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
