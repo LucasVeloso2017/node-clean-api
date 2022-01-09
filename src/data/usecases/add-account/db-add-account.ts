30966de1ddee495f61df23c139330829e7ba3c56
Lucas Veloso <lucas.velloso2017@gmail.com>
2022-01-09 20:21:31 -0300

    feat: add use case for addAccount

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,22 @@
+import { AddAccountRepository } from './../../protocols/add-account-repository'
+import { AccountModel } from '../../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../../domain/useCases/add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
+
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const encryptedPass = await this.encrypter.encrypt(accountData.password)
+
+    const account = await this.addAccountRepository.add({
+      ...accountData,
+      password: encryptedPass
+    })
+
+    return account
+  }
+}
