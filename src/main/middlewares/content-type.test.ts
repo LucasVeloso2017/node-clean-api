580ad82d85cf6a2b48211374b82bdd94eae8db71
Lucas Veloso <lucas.velloso2017@gmail.com>
2022-01-12 23:08:04 -0300

    test: add test for contentType

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import { app } from '../config/app'
+
+describe('Content Type', () => {
+  it('should return default content-type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+  it('should return content-type as xml forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
